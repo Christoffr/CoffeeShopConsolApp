4afe1e49222e3d54d063a54077c780c6da707654
Christoffer <bendigsunne@hotmail.com>
2026-09-01 09:47:39 +0200

    added coffee classes

---
+++ CoffeeShopConsoleAppNet60/Coffee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopConsoleAppNet60
+{
+    public class Coffee
+    {
+        public virtual decimal Price()
+        {
+            return 20;
+        }
+    }
+}
---
+++ CoffeeShopConsoleAppNet60/Cortado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopConsoleAppNet60
+{
+    public class Cortado : Coffee
+    {
+        public override decimal Price()
+        {
+            return base.Price() +5;
+        }
+    }
+}
