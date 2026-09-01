1714806a1cbe9d98a99c8bb8a1d2494488d873bb
Christoffer <bendigsunne@hotmail.com>
2026-09-01 10:20:44 +0200

    added a FlatWhite coffee

---
+++ CoffeeShopConsoleAppNet60/FlatWhite.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopConsoleAppNet60
+{
+    public class FlatWhite : Coffee, IMilk
+    {
+        public FlatWhite(int discount) : base(discount)
+        {
+            if (discount > 5)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+        public int mlMilk()
+        {
+            return 300;
+        }
+
+        public override decimal Price()
+        {
+            return base.Price() + 15 - base.discount;
+        }
+
+        public override string Strength()
+        {
+            return "Weak";
+        }
+    }
+}
--- CoffeeShopConsoleAppNet60/Program.cs
+++ CoffeeShopConsoleAppNet60/Program.cs
@@ -3,15 +3,21 @@
 BlackCoffee blackCoffee = new BlackCoffee(2);
 Cortado cortado = new Cortado(3);
 Latte latte = new Latte(5);
+FlatWhite flatWhite = new FlatWhite(4);
 
 
 List<Coffee> coffeeList = new List<Coffee> {blackCoffee, cortado, latte };
 
-List<IMilk> coffeesWithMilk = new List<IMilk> { cortado, latte };
+List<IMilk> coffeesWithMilk = new List<IMilk> { cortado, latte, flatWhite};
 
 foreach (var coffee in coffeeList)
 {
     Console.WriteLine($"{coffee.Price()}, {coffee.Strength()}");
+}
+
+foreach (var coffee in coffeesWithMilk)
+{
+    Console.WriteLine(coffee.mlMilk());
 }
 
 
